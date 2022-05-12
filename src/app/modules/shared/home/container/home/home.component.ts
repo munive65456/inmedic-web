9a20be997797279255a662d8814c5f1cdcc0e254
fmunive <andresmunive9906@gmail.com>
2022-05-12 18:32:57 -0500

    services page done

--- src/app/modules/shared/home/container/home/home.component.ts
+++ src/app/modules/shared/home/container/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  public page = "home";
+  public page = "services";
 
   constructor() { }
 
