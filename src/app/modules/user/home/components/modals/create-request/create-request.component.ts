9f236d8c0e975b53312c657ab30e51728cc57b40
Jose Daniel Fernandez <josedanielfer@gmail.com>
2022-05-21 23:05:16 -0500

    form with validations

--- src/app/modules/user/home/components/modals/create-request/create-request.component.ts
+++ src/app/modules/user/home/components/modals/create-request/create-request.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, NgControlStatus } from '@angular/forms';
-
 @Component({
   selector: 'app-create-request',
   templateUrl: './create-request.component.html',
@@ -9,15 +8,40 @@ import { FormGroup, FormBuilder, Validators, NgControlStatus } from '@angular/fo
 export class CreateRequestComponent implements OnInit {
 
   form: FormGroup;
+  printedOption: string;
 
-  constructor(private formBuilder: FormBuilder) { }
+  servicios = [
+    { name: "ortodonxia", value: 0 },
+    { name: "limpieza", value: 1 },
+    { name: "blanqueamiento", value: 2 }
+  ];
+
+  modalidades = [
+    {name: "presencial", value: 0},
+    {name: "virtual", value: 1}
+  ]
+
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      name: ['', [Validators.required]],
+      name: ['', Validators.required],
       cc: ['', Validators.required],
-      age: ['', Validators.required]
+      age: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+      description: [''],
+      servicioSelected: [null],
+      modalidadSelected: [null]
     });
+  }
+
+  
+  printServicio() {
+    console.log(this.servicios[this.form.get('servicioSelected')?.value].name);
+  }
+
+  printModalidad() {
+    console.log(this.modalidades[this.form.get('modalidadSelected')?.value].name);
   }
 
 }
