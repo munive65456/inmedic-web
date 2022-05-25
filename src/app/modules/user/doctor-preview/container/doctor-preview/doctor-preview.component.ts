921a202ce7172824ed3daa8758c545ccc2b2614a
Jose Daniel Fernandez <josedanielfer@gmail.com>
2022-05-25 01:35:33 -0500

    cards para review añadidas

--- src/app/modules/user/doctor-preview/container/doctor-preview/doctor-preview.component.ts
+++ src/app/modules/user/doctor-preview/container/doctor-preview/doctor-preview.component.ts
@@ -11,6 +11,15 @@ import { CreateRequestComponent } from 'src/app/modules/shared/modals/create-req
 })
 export class DoctorPreviewComponent implements OnInit {
 
+  reviews = [
+    { name: "Munive", date: "12-04-2022", text: "Ad nostrud dolore magna excepteur sit qui reprehenderit elit ad fugiat Lorem aute. Fugiat nulla cillum enim irure id magna ea eiusmod. Enim exercitation anim officia amet labore ad. Eiusmod tempor dolor occaecat quis incididunt aute. Tempor sunt aliquip fugiat ipsum ea minim quis fugiat. Sint ea veniam irure cillum ea sint Lorem ad.Id nulla commodo nisi est officia reprehenderit irure aliqua quis duis deserunt fugiat ad tempor." },
+    { name: "Daniel", date: "10-03-2022", text: "Cillum Lorem anim adipisicing est ullamco do. Aute cupidatat Lorem nulla anim proident nostrud ullamco mollit. Duis esse non enim do et magna cillum aliqua est nisi incididunt laboris eiusmod. Est officia reprehenderit in enim ipsum consequat eiusmod ut nulla dolor dolor. Incididunt laboris consequat ad sunt sunt consequat qui. Aliquip nisi cillum eiusmod officia eiusmod ex laboris pariatur in in aliqua nulla." },
+    { name: "Jose", date: "4-03-2022", text: "Aliqua eu in officia tempor. Enim minim sit ipsum aute. Eu cillum non tempor enim ipsum non ad cupidatat cupidatat officia velit. Anim Lorem consectetur pariatur consequat nulla in sint non." }
+  ];
+
+  
+
+
   constructor(private location: Location) { }
 
   @ViewChild('btnDetails')
