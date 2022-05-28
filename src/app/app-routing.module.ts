8c2ef23aaf1255bb007a1845d748c76006c0e662
Jose Daniel Fernandez <josedanielfer@gmail.com>
2022-05-28 00:43:23 -0500

    fix: problem with user route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,10 @@ const routes: Routes = [
     path: 'signup',
     loadChildren:() => import('./modules/signup/signup.module').then((m)=>m.SignupModule)
   },
-  
+  {
+    path: 'user',
+    loadChildren:() => import('./modules/user/user.module').then((m)=> m.UserModule)
+  },
   {
     path: 'doctor',
     loadChildren:()=> import('./modules/doctor/doctor.module').then((m)=> m.DoctorModule)
